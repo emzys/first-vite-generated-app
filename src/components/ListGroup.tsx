db164c37cc87def1bb8b9b8155b582c02816cc10
Emilia Zysk <emzysk.dev@gmail.com>
2023-05-09 15:42:29 +0900

    map items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,12 @@
 function ListGroup() {
+  const items = ["Warsaw", "Gdynia", "Cracow", "Olsztyn", "Yokohama"];
   return (
     <>
       <h1>Title</h1>
       <ul className="list-group">
-        <li className="list-group-item active">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
