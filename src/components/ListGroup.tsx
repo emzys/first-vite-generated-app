0899085991680d39a5ec6a3c65f6b696bc425cc2
Emilia Zysk <emzysk.dev@gmail.com>
2023-05-09 15:51:24 +0900

    getMessage

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,14 @@
 function ListGroup() {
-  const items = ["Warsaw", "Gdynia", "Cracow", "Olsztyn", "Yokohama"];
+  let items = ["Warsaw", "Gdynia", "Cracow", "Olsztyn", "Yokohama"];
+  items = [];
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found</p> : null;
+  };
+
   return (
     <>
       <h1>Title</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
