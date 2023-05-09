f224cc4214caa09d2add5545f01f92c2b5d2e7a2
Emilia Zysk <emzysk.dev@gmail.com>
2023-05-09 18:20:15 +0900

    event handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Warsaw", "Gdynia", "Cracow", "Olsztyn", "Yokohama"];
   // items = [];
@@ -5,13 +7,20 @@ function ListGroup() {
   //   return items.length === 0 && <p>No items found</p>;
   // };
 
+  // event handler
+  const handleClick = (e: MouseEvent) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>Title</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {index}. {item}
+          </li>
         ))}
       </ul>
     </>
