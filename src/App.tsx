aecdcc9b762a7594a1d43c666f19bec8b7e841b7
Emilia Zysk <emzysk.dev@gmail.com>
2023-05-11 17:02:20 +0900

    props childern

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
 
   return (
     <div className="App">
+      <Alert>
+        Hello <span>World</span>!
+      </Alert>
+
       {/* <ListGroup
         items={items}
         heading="Cities"
