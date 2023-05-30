4243034db527be63f462ee145c197f34b8653821
Emilia Zysk <emzysk.dev@gmail.com>
2023-05-30 16:57:34 +0900

    refactor

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -28,7 +28,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
-      <ul className={styles["list-group"]}>
+      <ul className={styles.listGroup}>
         {items.map((item, index) => (
           <li
             className={
